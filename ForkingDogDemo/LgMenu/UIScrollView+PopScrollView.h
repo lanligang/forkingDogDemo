6f67d23d0524980bfcc67f356515ca8cb7e2e192
LenSky <1176281703@qq.com>
2018-08-23 10:18:29 +0800

    # 处理有滚动视图横向滚动的首页不能侧滑展开的情况

---
+++ ForkingDogDemo/LgMenu/UIScrollView+PopScrollView.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+PopScrollView.h
+//  TeBrand
+//
+//  Created by ios2 on 2018/8/23.
+//  Copyright © 2018年 SunBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (PopScrollView)
+
+@end
---
+++ ForkingDogDemo/LgMenu/UIScrollView+PopScrollView.m
@@ -0,0 +1,23 @@
+//
+//  UIScrollView+PopScrollView.m
+//  TeBrand
+//
+//  Created by ios2 on 2018/8/23.
+//  Copyright © 2018年 SunBo. All rights reserved.
+//
+
+#import "UIScrollView+PopScrollView.h"
+
+@implementation UIScrollView (PopScrollView)
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+	if (self.contentOffset.x <= 0) {
+		if ([self.delegate isKindOfClass:NSClassFromString(@"LeftViewController")]) {
+			return NO;
+		}
+		if ([otherGestureRecognizer.delegate isKindOfClass:NSClassFromString(@"LgMenuViewController")]) {
+			return YES;
+		}
+	}
+	return NO;
+}
+@end
